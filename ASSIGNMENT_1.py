963ab0149ed3b1faa86eb24b3aedd4d26ae495c7
pandav086 <101456652+pandav086@users.noreply.github.com>
2022-04-28 10:47:53 +0530

    Add files via upload

---
+++ ASSIGNMENT_1.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+1. In the below elements which of them are values or an expression? eg:- values can be
+integer or string and expressions will be mathematical operators.
+* 
+hello
+-87.8
+-
+()
++
+6
+
+
+
+
+# In[ ]:
+
+
+ANSWER              
+
+
+*  - EXPRESSION
+
+hello -  value
+
+-87.8 -  VALUE
+
+-     EXPRESSION
+
+EXPRESSION()
+
++    EXPRESION
+
+6     VALUE
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('2. What is the difference between string and variable');get_ipython().run_line_magic('pinfo', 'variable')
+
+
+ANSWER - variable is a expression in which we store values or strings
+          string is type of information which we store in a variable
+    
+         a= 'ashutosh'
+        
+        here a is a variable and 'ashutosh' is a string stored in variable a.
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+3. Describe three different data types.
+
+
+
+ANSWER:  INTEGER  -    3
+          FLOAT  -      3.45
+        BOOLEANS -   TRUE /FALSE
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('4. What is an expression made up of? What do all expressions do');get_ipython().run_line_magic('pinfo', 'do')
+
+ANSWER:   expression is a combination of operator and operand which combination gives you a output.
+          example:- 
+            
+            a+b = 5
+            a-b =8
+            a*b =10
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+5. This assignment statements, like spam = 10. What is the difference between an
+get_ipython().set_next_input('expression and a statement');get_ipython().run_line_magic('pinfo', 'statement')
+
+
+ANSWER:-     expresion always gives you a ouput like 5+4 =9
+    
+         statement create a varibale like a= 10
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('6. After running the following code, what does the variable bacon contain');get_ipython().run_line_magic('pinfo', 'contain')
+bacon = 22
+bacon + 1
+
+
+ANSWER: 
+    
+    bacon = 22
+    bacon + 1 
+    
+    this means 22+1 = 23
+    
+    so new value of bacon will be 23
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('7. What should the values of the following two terms be');get_ipython().run_line_magic('pinfo', 'be')
+
+'spam'+'spamspam' -   do concatenation  means ' spamspamspam'
+
+'spam'*3  -   spamspamspam
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('8. Why is eggs a valid variable name while 100 is invalid');get_ipython().run_line_magic('pinfo', 'invalid')
+
+
+ANSWER:- because we cant start variable name with integer. to make it variable we should start with alphabet
+    like ab100 now its a variable.
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+9. What three functions can be used to get the integer, floating-point number, or string
+get_ipython().set_next_input('version of a value');get_ipython().run_line_magic('pinfo', 'value')
+
+ANSWER:-  str()
+          int()
+        float()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('10. Why does this expression cause an error? How can you fix it');get_ipython().run_line_magic('pinfo', 'it')
+'I have eaten'  + 99 + 'burritos'
+
+
+ANSWER:-   because 99 is a integer we cannot do concatabation with strings
+    
+    to do concatanation we need to do type casting
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+'I have eaten'  + str(99) + 'burritos'
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
